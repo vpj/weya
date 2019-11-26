21009c946cc6cd60f27f5aeb5615363794837e30
Varuna Jayasiri <vpjayasiri@gmail.com>
2019-11-26 13:13:50 +0530

    new weya and sample

--- sample.ts
+++ sample.ts
@@ -1,34 +1,12 @@
-import {Weya} from './main'
+import { Weya as $ } from './weya'
 
-Weya({ elem: this.elems.parent, context: this }, $ => {
-    $.div(".signin-base", $ => {
-        $.a(".google-sign", {
-            href: "#",
-            on: { click: this.onGoogleClick.bind(this) }
-        }, $ => {
-            $.img(".normal", {
-                src: "/assets/btn_google_signin_light_normal_web.png",
-                alt: "Signin with Google"
-            })
-            $.img(".focused", {
-                src: "/assets/btn_google_signin_light_focus_web.png",
-                alt: "Signin with Google"
-            })
-            $.img(".pressed", {
-                src: "/assets/btn_google_signin_light_pressed_web.png",
-                alt: "Signin with Google"
-            })
-        })
-        return $.div(".get-link", $ => {
-            $.p("Or get a signin link")
-            $.label("Email", { for: "email" })
-            this.elems.email = $.input("#email", {
-                type: "text",
-                placeholder: "Email"
-            })
-            $.button(".btn", "Get Signin Link", {
-                on: { click: this.getLinkClicked.bind(this) }
-            })
-        })
-    })
+let buttons = []
+
+$('div.content', document.body, $ => {
+    for(let i = 0; i < 10; ++i) {
+        buttons.push($('div.btn-holder', $ => {
+            $('p', 'Test')
+            $('button', `Button: ${i}`)
+        }))
+    }
 })
